d671702f3735f36158faf0d3942c684a58f3b3c5
Gashev-Alexander <gashev91@gmail.com>
2022-09-13 16:58:21 +0300

    написал программу именного приветствия для пайтона

---
+++ Python/python1.3.py
@@ -0,0 +1,17 @@
+a = 1
+b = 2
+c = 6
+d = 8
+e = 4
+max = a
+if a > max:
+    max = a
+if b > max:
+    max = b
+if c > max:
+    max = c
+if d > max:
+    max = d
+if e > max:
+    max = e
+print(max)
---
+++ Python/python1.4.py
@@ -0,0 +1,7 @@
+name = input("Введите ваше имя \n")
+username = name
+if name == 'Александр':
+    username = 'Санек'
+if name == 'Аделя':
+    username = 'Диля'
+print("Привет", username)
