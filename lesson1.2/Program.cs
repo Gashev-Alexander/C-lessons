34303bb3b96cbb3add1eaf31176bc93cbd946a58
Gashev-Alexander <gashev91@gmail.com>
2022-09-13 15:39:46 +0300

    написал программы сложения и деления на С и на пайтоне, добавил список папок в readme

---
+++ lesson1.2/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = 31;
+int numberВ = 5;
+int result = numberA + numberВ;
+Console.WriteLine(result);
---
+++ lesson1.3/Program.cs
@@ -0,0 +1,3 @@
+﻿int numberA = 10;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
