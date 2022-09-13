df25c06eb1c4b779dce4f7696328d371819951f0
Gashev-Alexander <gashev91@gmail.com>
2022-09-13 14:46:43 +0300

    какая то херота

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,3 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! Hello GeekBrains!");
-// Любой код, который написан после двух обратных («//») слэшей называется «комментарием к коду»
