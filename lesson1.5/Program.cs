ae202848b8617a6246c3ba090c083e6de0eaa2f2
Gashev-Alexander <gashev91@gmail.com>
2022-09-13 16:11:42 +0300

    программа именного приветствия не работает

---
+++ lesson1.5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+if(username.ToLower() == "Маша")
+{
+Сonsole.WriteLine(Ура, это же МАША);
+}
+else
+{
+Сonsole.Write("Привет, "); 
+Сonsole.WriteLine(username);
+}
