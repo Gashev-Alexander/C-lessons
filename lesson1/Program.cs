bde0f542f9e82c1866b902625abce531db332743
Gashev-Alexander <gashev91@gmail.com>
2022-09-13 14:35:13 +0300

    Создал дотнет консоль и мд файл для конспекта

---
+++ lesson1/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Hello GeekBrains!");
+// Любой код, который написан после двух обратных («//») слэшей называется «комментарием к коду»
