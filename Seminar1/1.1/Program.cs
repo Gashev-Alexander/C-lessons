3725ba62f2919fa0faaa438d060ab51d2cc8c96c
Gashev-Alexander <gashev91@gmail.com>
2022-09-15 20:48:37 +0300

    решение задач на семинаре

---
+++ Seminar1/1.1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.Write($"Квадрат числа {number} равняется {sqr}");
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2)); 
+// (number, Число) число указывает в какую сепень возвести
+Console.Write($" Квадрат числа {number} равняется {sqr1}");
---
+++ Seminar1/1.2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine());
+if (a * a == b) {
+    Console.Write("Yes");
+} else {
+    Console.Write("No");
+}
