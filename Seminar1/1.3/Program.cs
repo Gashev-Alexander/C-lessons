9ffa5496d18076f96541439d1ffad984751f9b31
Gashev-Alexander <gashev91@gmail.com>
2022-09-15 20:59:22 +0300

    решение задач на семинаре дополнил

--- Seminar1/1.3/Program.cs
+++ Seminar1/1.3/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if(number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if(number == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if(number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if(number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
