74172ac04fc1af422db1fc7b0584a8f2998fd47c
Gashev-Alexander <gashev91@gmail.com>
2022-09-26 01:04:10 +0300

    Сделана домашка, исправлены ошибки, изменено задание семинара 3.3 смотреть комит внутри

--- Seminar3/seminar3.3/Program.cs
+++ Seminar3/seminar3.3/Program.cs
@@ -2,9 +2,13 @@
 Console.WriteLine("Введите координаты точки А:");
 int xA = int.Parse(Console.ReadLine());
 int yA = int.Parse(Console.ReadLine());
+int zA = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите координаты точки B:");
 int xB = int.Parse(Console.ReadLine());
 int yB = int.Parse(Console.ReadLine());
-double num = Math.Pow((xB - xA), 2) + Math.Pow((yB - yA), 2);
+int zB = int.Parse(Console.ReadLine());
+double num = Math.Pow((xB-xA), 2) + Math.Pow((yB-yA), 2) + Math.Pow((zB-zA), 2);
 double AB = Math.Sqrt(num);
 Console.Write($"Длина отрезка => {AB.ToString("0.000")}");
+
+// программа переписана для вычесления растояния между двумя точка в 3D простанстве
