59baa890039fe5427ec0cad915822862d719370e
Gashev-Alexander <gashev91@gmail.com>
2022-09-29 01:47:12 +0300

    Добавил анг.яз в код по русному и автоматическому заполнению массива

--- Arrays/Compilation v2/Program.cs
+++ Arrays/Compilation v2/Program.cs
@@ -1,23 +1,26 @@
 ﻿Console.Clear();
-Console.Write($"Выберите тип заполнения массива (Ручной/Автоматический)\nОтвет: ");
+Console.Write($"Выберите тип заполнения массива (Ручной/Автоматический)");
+Console.Write($"\nSelect the type of array filling (Manual/Automatic)\nОтвет/Answer:");
 switch(Console.ReadLine())
 {
     case "Ручной":
     case "ручной":
+    case "Manual":
+    case "manual":
     case "рука":
     case "Рука":
     case "Hand":
     case "hand":
         Console.Clear();
-        Console.Write("Введите размер массива: ");
+        Console.Write("Введите размер массива\nEnter the size of the array: ");
         int SizeMas = int.Parse(Console.ReadLine());
         int[] array = new int[SizeMas];
         for (int i = 0; i < array.Length; i++)
         {
-            Console.Write($"Введите элемент массива с индексом {i} : ");
+            Console.Write($"Введите элемент массива с индексом {i}\n      Enter array element at index {i} : ");
             array[i] = int.Parse(Console.ReadLine());
         }
-            Console.WriteLine("\nВывод массива:");
+            Console.WriteLine("\nВывод массива\nArray Output:");
             void TopArray(int[] coll)
         {
             int count = coll.Length;
@@ -44,8 +47,10 @@ switch(Console.ReadLine())
     case "авто":
     case "auto":
     case "Auto":
+    case "automatic":
+    case "Automatic":
         Console.Clear();
-        Console.Write("Введите размер массива: ");
+        Console.Write("Введите размер массива\nEnter the size of the array: ");
         int SizeMas1 = int.Parse(Console.ReadLine());
         int[] array1 = GetBinaryArray(SizeMas1);
         Console.WriteLine($"[{String.Join(",", array1)}]");
@@ -61,14 +66,16 @@ switch(Console.ReadLine())
         }
     break;    
 
+    case "Ногой":
+    case "ногой":
     case "Нога":
     case "нога":
     case "Leg":
     case "leg":
-        Console.WriteLine("Я смотрю ты Гимнаст=)");
+        Console.WriteLine("Я смотрю ты Гимнаст=)\nI see you are a gymnast =)");
     break;
 
     default:
-        Console.WriteLine("Ошибка ввода, попробуй еще раз!");
+        Console.WriteLine("Ошибка ввода, попробуй еще раз!\nInput error, please try again!");
     break;
 }
