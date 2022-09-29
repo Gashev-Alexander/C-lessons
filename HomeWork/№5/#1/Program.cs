51f44944287a67480c274550309891d9a8878b66
Gashev-Alexander <gashev91@gmail.com>
2022-09-29 22:29:51 +0300

    семинар 5 половину программ пропустил, надо догнать. сделал 1 задание дз

---
+++ HomeWork/№5/#1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+int count = 0;
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,999);
+    }
+}
+Console.Clear();
+void TopArray(int[] coll)
+{
+    int count = coll.Length;
+    int i = 0;
+    Console.Write("[");
+    while(i < count)
+{
+        Console.Write(coll[i]);
+        i++;
+        if (i < count)
+    {
+        Console.Write(", ");
+    }
+}
+  Console.Write("]");
+}
+TopArray(numbers);
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+Console.WriteLine($"\n{count} чисел из {numbers.Length} чётные.");
---
+++ Seminar5/Seminar5.1/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Clear();
+Console.Write("Введите размер массива: ");
+int SizeMas = int.Parse(Console.ReadLine());
+int[] array = GetBinaryArray(SizeMas);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int[] GetBinaryArray(int size)
+    {
+        int[] result = new int  [size];
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = new Random().Next(-9,9);
+        }
+    return result;
+    }
+
+// сумма отрицательных и положительных чисел
+int plus = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i] > 0)
+plus += array[i]; 
+Console.Write("Сумма положительных чисел: ");
+Console.WriteLine(plus);
+
+int minus = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i] < 0)
+minus += array[i]; 
+Console.Write("Сумма отрицательных чисел: ");
+Console.WriteLine(minus);
+
+
+// противоположность массива
+for (int i = array.Length - 1; i > array.Length * (-1); i--)
+            array[i] = -1*array[i];
+            
+        Console.Write("Изменённый массив: ");
+         for (int i = 0; i < array.Length; i++)
+            Console.Write((i != array.Length - 1) ? $"{array[i]}; " : $"{array[i]}\n");
+            
+        Console.ReadKey();
